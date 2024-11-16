56068f20bf9d8703f3c729c812d1dfd8802cd3e4
unknown <trungkienjoe58@gmail.com>
2024-11-16 19:08:55 +0700

    LAB 4 all function tested

--- Embedded-System-LAB-LAB4/LAB4/Core/Src/fsm.c
+++ Embedded-System-LAB-LAB4/LAB4/Core/Src/fsm.c
@@ -8,6 +8,13 @@
 #include "fsm.h"
 #include "global.h"
 
+/*
+ * button_count[0] is button with label "1" on the kit
+ * button_count[3] is button with label "↑" on the kit
+ * button_count[14] is button with label "B" on the kit
+ */
+
+
 uint8_t count_increase = 0;
 uint8_t count_led_flash = 0;
 uint8_t flag = 0;
@@ -56,7 +63,6 @@ void displayAdjTime(){
 }
 
 void displayTimer(){
-//	lcd_Clear(BLACK);
 	lcd_ShowIntNum(70, 100, timer_hours, 2, MAGENTA, BLACK, 24);
 	lcd_ShowIntNum(110, 100, timer_min, 2, MAGENTA, BLACK, 24);
 	lcd_ShowIntNum(150, 100, timer_sec, 2, MAGENTA, BLACK, 24);
@@ -71,7 +77,6 @@ void clockTime(){
 	ds3231_Write(ADDRESS_DAY, ds3231_day);
 }
 
-
 void timer_mode(){
 	switch(TIMER){
 	case init:
@@ -83,66 +88,59 @@ void timer_mode(){
 			lcd_ShowIntNum(70, 100, timer_hours, 2, BLACK, BLACK, 24);
 		} else lcd_ShowIntNum(70, 100, timer_hours, 2, MAGENTA, BLACK, 24);
 		if(button_count[3] == 1) {
-			timer_hours = (timer_hours + 1) %24; //btn3 = ↑
+			timer_hours = (timer_hours + 1) %24;
 			ds3231_Write(ADDRESS_HOUR, timer_hours);
 		}
 		if (button_count[3] >= 40) flag = 1; else flag = 0;
 		if (button_count[3] % 4 == 1 && flag == 1) {
 			count_led_flash = 11;
-//			HAL_GPIO_TogglePin(DEBUG_LED_GPIO_Port, DEBUG_LED_Pin);
-			timer_hours = (timer_hours + 1) %24; //btn3 = ↑
+			timer_hours = (timer_hours + 1) %24;
 			ds3231_Write(ADDRESS_HOUR, timer_hours);
 		}
-
 		if(button_count[0] == 1) {TIMER = timerM; count_led_flash = 0; displayTimer();}
-		// if(button_count[14] == 1) status = MODE1;
 		break;
 	case timerM:
 		if(count_led_flash < 10){
 			lcd_ShowIntNum(110, 100, timer_min, 2, BLACK, BLACK, 24);
 		} else lcd_ShowIntNum(110, 100, timer_min, 2, MAGENTA, BLACK, 24);
 		if(button_count[3] == 1) {
-			timer_min = (timer_min + 1) % 60; //btn3 = ↑
+			timer_min = (timer_min + 1) % 60;
 			ds3231_Write(ADDRESS_MIN, timer_min);
 		}
 		if (button_count[3] >= 40) flag = 1; else flag = 0;
 		if (button_count[3] % 4 == 1 && flag == 1) {
 			count_led_flash = 11;
-//			HAL_GPIO_TogglePin(DEBUG_LED_GPIO_Port, DEBUG_LED_Pin);
-			timer_min = (timer_min + 1) % 60; //btn3 = ↑
+			timer_min = (timer_min + 1) % 60;
 			ds3231_Write(ADDRESS_MIN, timer_min);
 		}
 		if(button_count[0] == 1) {TIMER = timerS; count_led_flash = 0; displayTimer();}
-		// if(button_count[14] == 1) status = MODE1;
 		break;
 	case timerS:
 		if(count_led_flash < 10){
 			lcd_ShowIntNum(150, 100, timer_sec, 2, BLACK, BLACK, 24);
 		} else lcd_ShowIntNum(150, 100, timer_sec, 2, MAGENTA, BLACK, 24);
 		if(button_count[3] == 1) {
-			timer_sec = (timer_sec + 1) % 60; //btn3 = ↑
+			timer_sec = (timer_sec + 1) % 60;
 			ds3231_Write(ADDRESS_SEC, timer_sec);
 		}
 		if (button_count[3] >= 40) flag = 1; else flag = 0;
 		if (button_count[3] % 4 == 1 && flag == 1) {
 			count_led_flash = 11;
-//			HAL_GPIO_TogglePin(DEBUG_LED_GPIO_Port, DEBUG_LED_Pin);
-			timer_sec = (timer_sec + 1) % 60; //btn3 = ↑
+
+			timer_sec = (timer_sec + 1) % 60;
 			ds3231_Write(ADDRESS_SEC, timer_sec);
 		}
 		if(button_count[0] == 1) {TIMER = timerC; count_led_flash = 0; displayTimer();}
-		// if(button_count[14] == 1) status = MODE1;
 		break;
 	case timerC:
 		if(count_led_flash < 10){
 			lcd_ShowIntNum(20, 100, timer_check, 2, BLACK, BLACK, 24);
 		} else lcd_ShowIntNum(20, 100, timer_check, 2, MAGENTA, BLACK, 24);
 		if(button_count[3] == 1) {
-			timer_check = (timer_check + 1) %2;//btn3 = ↑
+			timer_check = (timer_check + 1) %2;
 			ds3231_Write(ADDRESS_DAY, timer_check);
 		}
 		if(button_count[0] == 1) {TIMER = timerH; count_led_flash = 0; displayTimer();}
-		// if(button_count[14] == 1) status = MODE1;
 		break;
 	default:
 		break;
@@ -165,73 +163,62 @@ void alarm(){
 		displayTime();
 	}
 }
+
 void adjustingTime_mode(){
 	switch(TMP){
 	case hour:
-//		displayTime();
 		if(count_led_flash < 10){
 			lcd_ShowIntNum(70, 100, ds3231_hours, 2, BLACK, BLACK, 24);
 		} else lcd_ShowIntNum(70, 100, ds3231_hours, 2, GREEN, BLACK, 24);
 
 		if (button_count[3] == 1) {
-		    // Increment hour and ensure it's in 24-hour format
-		    ds3231_hours = (ds3231_hours + 1) %24; // Increment and wrap around after 24
-		    ds3231_Write(ADDRESS_HOUR, ds3231_hours); // Update RTC module with the new hour
+		    ds3231_hours = (ds3231_hours + 1) %24;
+		    ds3231_Write(ADDRESS_HOUR, ds3231_hours);
 		}
 		if (button_count[3] >= 40) flag = 1;
 		else flag = 0;
 		if (button_count[3] % 4 == 1 && flag == 1) {
 			count_led_flash = 11;
-//			HAL_GPIO_TogglePin(DEBUG_LED_GPIO_Port, DEBUG_LED_Pin);
-			ds3231_hours = (ds3231_hours + 1) %24; // Increment and wrap around after 24
+
+			ds3231_hours = (ds3231_hours + 1) %24;
 			ds3231_Write(ADDRESS_HOUR, ds3231_hours);
 		}
-
 		if(button_count[0] == 1) {TMP = min; count_led_flash = 0; displayAdjTime(); }
-//		if(button_count[14] == 1) status = MODE3;
 		break;
 	case min:
-//		displayTime();
 		if(count_led_flash < 10){
 			lcd_ShowIntNum(110, 100, ds3231_min, 2, BLACK, BLACK, 24);
 		} else lcd_ShowIntNum(110, 100, ds3231_min, 2, GREEN, BLACK, 24);
 		if(button_count[3] == 1) {
-			ds3231_min = (ds3231_min + 1) % 60; //btn3 = ↑
+			ds3231_min = (ds3231_min + 1) % 60;
 			ds3231_Write(ADDRESS_MIN, ds3231_min);
 		}
 		if (button_count[3] >= 40) flag = 1; else flag = 0;
 		if (button_count[3] % 4 == 1 && flag == 1) {
 			count_led_flash = 11;
-//			HAL_GPIO_TogglePin(DEBUG_LED_GPIO_Port, DEBUG_LED_Pin);
-			ds3231_min = (ds3231_min + 1) % 60; //btn3 = ↑
+			ds3231_min = (ds3231_min + 1) % 60;
 			ds3231_Write(ADDRESS_MIN, ds3231_min);
 		}
-
-
 		if(button_count[0] == 1) {TMP = sec; count_led_flash = 0; displayAdjTime();}
-//		if(button_count[14] == 1) status = MODE3;
 		break;
 	case sec:
-//		displayTime();
 		if(count_led_flash < 10){
 			lcd_ShowIntNum(150, 100, ds3231_sec, 2, BLACK, BLACK, 24);
 		} else lcd_ShowIntNum(150, 100, ds3231_sec, 2, GREEN, BLACK, 24);
 		if(button_count[3] == 1) {
-			ds3231_sec = (ds3231_sec + 1) % 60; //btn3 = ↑
+			ds3231_sec = (ds3231_sec + 1) % 60;
 			ds3231_Write(ADDRESS_SEC, ds3231_sec);
 		}
 		if (button_count[3] >= 40) flag = 1; else flag = 0;
 		if (button_count[3] % 4 == 1 && flag == 1) {
 			count_led_flash = 11;
-//			HAL_GPIO_TogglePin(DEBUG_LED_GPIO_Port, DEBUG_LED_Pin);
-			ds3231_sec = (ds3231_sec + 1) % 60; //btn3 = ↑
+
+			ds3231_sec = (ds3231_sec + 1) % 60;
 			ds3231_Write(ADDRESS_SEC, ds3231_sec);
 		}
 		if(button_count[0] == 1) {TMP = day; count_led_flash = 0; displayAdjTime();}
-//		if(button_count[14] == 1) status = MODE3;
 		break;
 	case day:
-//		displayTime();
 		if(count_led_flash < 10){
 			lcd_ShowIntNum(20, 130, ds3231_day, 2, BLACK, BLACK, 24);
 			lcd_ShowIntNum(70, 130, ds3231_date, 2, BLACK, BLACK, 24);
@@ -240,7 +227,7 @@ void adjustingTime_mode(){
 			lcd_ShowIntNum(70, 130, ds3231_date, 2, YELLOW, BLACK, 24);
 		}
 		if(button_count[3] == 1) {
-			ds3231_day = (ds3231_day % 7) + 1;//btn3 = ↑ //day = 6 -> 7 = 0, display: 1?
+			ds3231_day = (ds3231_day % 7) + 1;
 			ds3231_date = (ds3231_date % 30) + 1;
 			ds3231_Write(ADDRESS_DAY, ds3231_day + 2);
 			ds3231_Write(ADDRESS_DATE, ds3231_date);
@@ -248,52 +235,44 @@ void adjustingTime_mode(){
 		if (button_count[3] >= 40) flag = 1; else flag = 0;
 		if (button_count[3] % 4 == 1 && flag == 1) {
 			count_led_flash = 11;
-//			HAL_GPIO_TogglePin(DEBUG_LED_GPIO_Port, DEBUG_LED_Pin);
-			ds3231_day = (ds3231_day % 7) + 1;//btn3 = ↑ //day = 6 -> 7 = 0, display: 1?
+			ds3231_day = (ds3231_day % 7) + 1;
 			ds3231_date = (ds3231_date % 30) + 1;
 			ds3231_Write(ADDRESS_DAY, ds3231_day + 2);
 			ds3231_Write(ADDRESS_DATE, ds3231_date);
 		}
 		if(button_count[0] == 1) {TMP = month; count_led_flash = 0; displayAdjTime();}
-//		if(button_count[14] == 1) status = MODE3;
 		break;
 	case month:
-//		displayTime();
 		if(count_led_flash < 10){
 			lcd_ShowIntNum(110, 130, ds3231_month, 2, BLACK, BLACK, 24);
 		} else lcd_ShowIntNum(110, 130, ds3231_month, 2, YELLOW, BLACK, 24);
 		if(button_count[3] == 1) {
-			ds3231_month = (ds3231_month % 12) +  1; //btn3 = ↑
+			ds3231_month = (ds3231_month % 12) +  1;
 			ds3231_Write(ADDRESS_MONTH, ds3231_month);
 		}
 		if (button_count[3] >= 40) flag = 1; else flag = 0;
 		if (button_count[3] % 4 == 1 && flag == 1) {
 			count_led_flash = 11;
-//			HAL_GPIO_TogglePin(DEBUG_LED_GPIO_Port, DEBUG_LED_Pin);
-			ds3231_month = (ds3231_month % 12) +  1; //btn3 = ↑
+			ds3231_month = (ds3231_month % 12) +  1;
 			ds3231_Write(ADDRESS_MONTH, ds3231_month);
 		}
 		if(button_count[0] == 1) {TMP = year; count_led_flash = 0; displayAdjTime();}
-//		if(button_count[14] == 1) status = MODE3;
 		break;
 	case year:
-//		displayTime();
 		if(count_led_flash < 10){
 			lcd_ShowIntNum(150, 130, ds3231_year, 2, BLACK, BLACK, 24);
 		} else lcd_ShowIntNum(150, 130, ds3231_year, 2, YELLOW, BLACK, 24);
 		if(button_count[3] == 1) {
-			ds3231_year = (ds3231_year % 99) +  1; //btn3 = ↑
+			ds3231_year = (ds3231_year % 99) +  1;
 			ds3231_Write(ADDRESS_YEAR, ds3231_year);
 		}
 		if (button_count[3] >= 40) flag = 1; else flag = 0;
 		if (button_count[3] % 4 == 1 && flag == 1) {
 			count_led_flash = 11;
-//			HAL_GPIO_TogglePin(DEBUG_LED_GPIO_Port, DEBUG_LED_Pin);
-			ds3231_year = (ds3231_year % 99) +  1; //btn3 = ↑
+			ds3231_year = (ds3231_year % 99) +  1;
 			ds3231_Write(ADDRESS_YEAR, ds3231_year);
 		}
 		if(button_count[0] == 1) {TMP = hour; count_led_flash = 0; displayAdjTime();}
-//		if(button_count[14] == 1) status = MODE3;
 		break;
 	default:
 		break;
@@ -304,12 +283,10 @@ void  fsm_run(){
 	switch(status){
 	case INIT:
 		updateTime();
-//		testAlarm();
-//		ds3231_ReadTime();
+//		testAlarm();       setup for arlam testing
 		status = MODE1;
 		break;
-	case MODE1: //running
-//		displayTime();
+	case MODE1: //done and running
 		alarm();
 		if(button_count[14] == 1) {
 			lcd_Clear(BLACK);
@@ -318,8 +295,7 @@ void  fsm_run(){
 			displayAdjTime();
 		}
 		break;
-	case MODE2: //running, the hold btn to increase not done yet
-//		count_led_flash = (count_led_flash + 1)%20;
+	case MODE2: //done and running
 		adjustingTime_mode();
 		if(button_count[14] == 1) {
 			status = MODE3;
@@ -327,18 +303,15 @@ void  fsm_run(){
 			TIMER = init;
 		}
 		break;
-	case MODE3: //running, hold btn to increase and alarm not done yet
-
+	case MODE3: //done and running
 		timer_mode();
 		if(button_count[14] == 1) {
 			status = MODE1;
 			lcd_Clear(BLACK);
 			clockTime();
-//			alarm();
 		}
 		break;
 	default:
 		break;
 	}
-	
 }
